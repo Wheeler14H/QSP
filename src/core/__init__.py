b1fae9394642f0dc53c12c7ea6f512ce0225a1e1
Wheeler14H <y676495@gmail.com>
2026-05-20 00:24:54 +0800

    Update __init__.py

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,5 +1,6 @@
 # core模块初始化文件
 
 from .messages import RecoveryMessage, RecoveryMsgType
+from .challenge_auth import ChallengeManager, build_auth_payload
 
-__all__ = ['RecoveryMessage', 'RecoveryMsgType']
+__all__ = ['RecoveryMessage', 'RecoveryMsgType', 'ChallengeManager', 'build_auth_payload']
